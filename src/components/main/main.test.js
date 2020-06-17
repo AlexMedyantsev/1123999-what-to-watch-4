c1fbca9c5910652ff962bc48e91f451b2609a527
AlexMedyantsev <medyantsev1978@icloud.com>
2020-06-17 16:33:25 +0300

    Добавил снепшот тесты

---
+++ src/components/main/main.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import rerender from "react-test-renderer";
+import Main from "./main.jsx";
+
+const HeaderMovieData = {
+  TITLE: `Movie Title`,
+  GENRE: `Drama`,
+  YEAR: 2009,
+};
+
+const MOVIES = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Avatar`, `Aviator`, `Pulp fiction`];
+
+describe(`Render component`, () => {
+  it(`Should Main render correctly`, () => {
+    const tree = rerender
+      .create(<Main
+        title={HeaderMovieData.title}
+        genre={HeaderMovieData.genre}
+        year={HeaderMovieData.year}
+        movies={MOVIES}
+      />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
