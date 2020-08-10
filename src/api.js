5803f4f86464ac45d75cdf738d4d0f88f28607ad
AlexMedyantsev <medyantsev1978@icloud.com>
2020-08-10 18:24:51 +0300

    Поправил логику отображения ошибки

--- src/api.js
+++ src/api.js
@@ -2,12 +2,11 @@ import axios from "axios";
 import {connect} from "react-redux";
 import {ActionCreator} from "./reducer/condition/condition.js";
 
-const Error = {
+const ErrorCodes = {
   UNAUTHORIZED: 401,
 };
 
-
-const createAPI = (onError) => {
+const createAPI = (onError, onUnauthorized) => {
   const api = axios.create({
     baseURL: `https://4.react.pages.academy/wtw`,
     timeout: 1000 * 5,
@@ -22,13 +21,15 @@ const createAPI = (onError) => {
     const {response} = err;
     if (response) {
       onError(response.status);
-    } else if (response.status === Error.UNAUTHORIZED) {
-      onError(`Вы не авторизированы`);
-    } else {
-      onError(`Отсутствует интернет подключение`);
+    } else if (!response) {
+      onError(`Отсутствует интернет соединение`);
     }
 
-    
+    if (response) {
+      if (response.status === ErrorCodes.UNAUTHORIZED) {
+        onUnauthorized(response);
+      }
+    }
   };
 
   api.interceptors.response.use(onSuccess, onFail);
--- src/index.js
+++ src/index.js
@@ -5,17 +5,27 @@ import reducer from "./reducer/reducer.js";
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Operation} from "./reducer/data/data.js";
 import {ActionCreator as ActionCreatorCondition} from "./reducer/condition/condition.js";
-import {Operation as UserOperation} from "./reducer/user/user.js";
+import {Operation as UserOperation, ActionCreator as ActionCreatorUser} from "./reducer/user/user.js";
 import App from "./components/app/app.jsx";
+import {AuthorizationStatus} from "./reducer/user/user.js";
 import thunk from 'redux-thunk';
 import {createAPI} from "./api.js";
+import history from "./history.js";
+
 
 const onError = (responce) => {
   store.dispatch(ActionCreatorCondition.setErrorMessage(responce));
   store.dispatch(ActionCreatorCondition.changeErrorFlag());
 };
 
-const api = createAPI(onError);
+const onUnauthorized = (error) => {
+  store.dispatch(ActionCreatorUser.requireAuthorization(AuthorizationStatus.NO_AUTH));
+  if (error.config.url !== `/login`) {
+    history.push(`/login`);
+  }
+};
+
+const api = createAPI(onError, onUnauthorized);
 
 const store = createStore(
     reducer,
